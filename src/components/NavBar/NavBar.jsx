1b28b118e08f823a590182cb20eb1d6d8ed10254
“Peter-Akasi-Mba” <awintechworld@gmail.com>
2024-04-15 15:58:41 +0000

    working on static navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,32 +5,35 @@ import { HiBars3 } from "react-icons/hi2";
 const NavBar = () => {
   return (
     <>
-      <div>
-        <h1>
-          <span>e</span>Buy
-        </h1>
+      <div className="flex items-center justify-between py-4 px-8">
+        <div>
+          <h1 className="text-xl font-extrabold">
+            <span className="text-orange-500">e</span>Buy
+          </h1>
+        </div>
+        <div className="hidden gap-3 text-sm text-orange-500 font-bold">
+          <div>
+            <a href="">About</a>
+          </div>
+          <div>
+            <a href="">Contact Us</a>
+          </div>
+        </div>
+        <div className="flex gap-10">
+          <div>
+            <FaHeart className="text-orange-500 cursor-pointer text-xl" />
+          </div>
+          <div>
+            <RiShoppingCartFill className="text-orange-500 cursor-pointer text-xl" />
+          </div>
+        </div>
+        <div>
+          <div>
+            <HiBars3 className="text-orange-500 cursor-pointer text-xl" />
+          </div>
+        </div>
       </div>
-      <div>
-        <div>
-          <a href="">About</a>
-        </div>
-        <div>
-          <a href="">Contact Us</a>
-        </div>
-      </div>
-      <div>
-        <div>
-          <FaHeart />
-        </div>
-        <div>
-          <RiShoppingCartFill />
-        </div>
-      </div>
-      <div>
-        <div>
-          <HiBars3 />
-        </div>
-      </div>
+      <hr />
     </>
   );
 };
