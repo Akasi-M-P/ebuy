bbc21af63ebb95b9f87a927577ef621ec4242d7d
“Peter-Akasi-Mba” <awintechworld@gmail.com>
2024-04-16 15:25:09 +0000

    working on footer in mobile view

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,38 @@
 import "./Footer.css";
+import { FaXTwitter, FaSquareInstagram, FaLinkedin } from "react-icons/fa6";
+import { FaFacebookSquare } from "react-icons/fa";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <>
+      <div className="footer bg-orange-500 text-white text-lg h-64 flex items-center">
+        <div className="mx-auto flex flex-col gap-4">
+          <div>
+            <a href="">About</a>
+          </div>
+          <div>
+            <a href="">Contact Us</a>
+          </div>
+          <div className="flex gap-2">
+            <FaXTwitter className="text-xl" />
+            <FaFacebookSquare className="text-xl" />
+            <FaSquareInstagram className="text-xl" />
+            <FaLinkedin className="text-xl" />
+          </div>
+        </div>
+        <div className="mx-auto flex flex-col gap-4">
+          <div>
+            <a href="">Payments</a>
+          </div>
+          <div>
+            <a href="">Delivery</a>
+          </div>
+          <div>
+            <a href="">FAQs</a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 export default Footer;
