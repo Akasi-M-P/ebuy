bd10ecec8ec9f152de5986ff3d42038b0c9d45ba
“Peter-Akasi-Mba” <awintechworld@gmail.com>
2024-04-18 23:49:40 +0000

    worked on cart responsiveness

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -6,11 +6,13 @@ import { FaWindowClose } from "react-icons/fa";
 const Cart = ({ OnOpenCart }) => {
   return (
     <>
-      <div className="h-screen" data-aos="flip-up">
-        <div className="w-full flex justify-between p-5">
-          <p className="font-bold text-orange-500">Your Cart Items</p>
+      <div className="h-screen lg:h-full" data-aos="flip-up">
+        <div className="w-full flex justify-between p-5 md:w-10/12 md:mx-auto">
+          <p className="font-bold text-orange-500 text-sm md:text-lg lg:text-2xl">
+            Your Cart Items
+          </p>
           <FaWindowClose
-            className=" text-orange-500 text-lg"
+            className=" text-orange-500 text-lg md:text-2xl lg:text-4xl cursor-pointer"
             onClick={OnOpenCart}
           />
         </div>
@@ -18,19 +20,21 @@ const Cart = ({ OnOpenCart }) => {
           <CartItem />
         </div>
         <div className="m-4">
-          <p className="text-sm text-center font-bold">
+          <p className="text-sm text-center font-bold md:text-xl lg:text-2xl">
             <span>Subtotal : </span>$ 32000.00
           </p>
         </div>
         <div className="w-full flex justify-center">
-          <button className="border p-2  w-2/3 rounded-xl bg-orange-600">
-            <p className="text-center text-lg text-white font-bold">
+          <button className="border p-3  w-2/3 rounded-xl bg-orange-600">
+            <p className="text-center text-lg text-white font-bold md:text-xl lg:text-2xl">
               Proceed to checkout
             </p>
           </button>
         </div>
-        <div className="w-full flex justify-center m-4">
-          <button className="text-sm text-red-400">Clear cart</button>
+        <div className="w-full flex justify-center m-4 lg:m-8">
+          <button className="text-sm text-red-400 md:text-lg">
+            Clear cart
+          </button>
         </div>
       </div>
     </>
--- src/components/Cart/CartItem.jsx
+++ src/components/Cart/CartItem.jsx
@@ -1,104 +1,136 @@
 const CartItem = () => {
   return (
     <>
-      <div className="flex items-center gap-10 px-4">
+      <div className="flex items-center gap-10 px-4 md:w-10/12 md:mx-auto md:gap-20 lg:justify-center lg:gap-32">
         <div className="w-2/5">
-          <img src="/assets/s22.png" alt="" />
+          <img src="/assets/s22.png" alt="" className="md:h-48" />
         </div>
         <div className="flex flex-col gap-10 py-4">
           <div>
-            <p className="font-bold text-sm">Samsung s22</p>
+            <p className="font-bold text-sm md:text-lg lg:text-2xl">
+              Samsung s22
+            </p>
           </div>
           <div>
-            <p className="font-bold text-orange-500 text-sm">$ 1200</p>
+            <p className="font-bold text-orange-500 text-sm md:text-lg lg:text-2xl">
+              $ 1200
+            </p>
           </div>
         </div>
         <div className="flex flex-col items-center gap-10">
           <div>
-            <button className="text-sm text-red-300">remove</button>
+            <button className="text-sm text-red-300 md:text-lg lg:text-2xl">
+              remove
+            </button>
           </div>
           <div className="flex border justify-between items-center gap-2 p-0 rounded-md ">
-            <button className="border px-2 text-lg rounded-sm">-</button>
-            <p>1</p>
-            <button className="border border-gray-400 px-2 text-lg rounded-sm">
+            <button className="border px-2 text-lg rounded-sm lg:text-2xl">
+              -
+            </button>
+            <p className="lg:text-2xl">1</p>
+            <button className="border border-gray-400 px-2 text-lg rounded-sm lg:text-2xl">
               +
             </button>
           </div>
         </div>
       </div>
       <hr />
-      <div className="flex items-center gap-10 px-4">
+      <div className="flex items-center gap-10 px-4 md:w-10/12 md:mx-auto md:gap-20 lg:justify-center lg:gap-32">
         <div className="w-2/5">
-          <img src="/assets/doublespeaker.png" alt="" />
+          <img src="/assets/s22.png" alt="" className="md:h-48" />
         </div>
         <div className="flex flex-col gap-10 py-4">
           <div>
-            <p className="font-bold text-sm">Samsung s22</p>
+            <p className="font-bold text-sm md:text-lg lg:text-2xl">
+              Samsung s22
+            </p>
           </div>
           <div>
-            <p className="font-bold text-orange-500 text-sm">$ 1200</p>
+            <p className="font-bold text-orange-500 text-sm md:text-lg lg:text-2xl">
+              $ 1200
+            </p>
           </div>
         </div>
         <div className="flex flex-col items-center gap-10">
           <div>
-            <button className="text-sm text-red-300">remove</button>
+            <button className="text-sm text-red-300 md:text-lg lg:text-2xl">
+              remove
+            </button>
           </div>
           <div className="flex border justify-between items-center gap-2 p-0 rounded-md ">
-            <button className="border px-2 text-lg rounded-sm">-</button>
-            <p>1</p>
-            <button className="border border-gray-400 px-2 text-lg rounded-sm">
+            <button className="border px-2 text-lg rounded-sm lg:text-2xl">
+              -
+            </button>
+            <p className="lg:text-2xl">1</p>
+            <button className="border border-gray-400 px-2 text-lg rounded-sm lg:text-2xl">
               +
             </button>
           </div>
         </div>
       </div>
       <hr />
-      <div className="flex items-center gap-10 px-4">
+      <div className="flex items-center gap-10 px-4 md:w-10/12 md:mx-auto md:gap-20 lg:justify-center lg:gap-32">
         <div className="w-2/5">
-          <img src="/assets/victuslaptop.png" alt="" />
+          <img src="/assets/s22.png" alt="" className="md:h-48" />
         </div>
         <div className="flex flex-col gap-10 py-4">
           <div>
-            <p className="font-bold text-sm">Samsung s22</p>
+            <p className="font-bold text-sm md:text-lg lg:text-2xl">
+              Samsung s22
+            </p>
           </div>
           <div>
-            <p className="font-bold text-orange-500 text-sm">$ 1200</p>
+            <p className="font-bold text-orange-500 text-sm md:text-lg lg:text-2xl">
+              $ 1200
+            </p>
           </div>
         </div>
         <div className="flex flex-col items-center gap-10">
           <div>
-            <button className="text-sm text-red-300">remove</button>
+            <button className="text-sm text-red-300 md:text-lg lg:text-2xl">
+              remove
+            </button>
           </div>
           <div className="flex border justify-between items-center gap-2 p-0 rounded-md ">
-            <button className="border px-2 text-lg rounded-sm">-</button>
-            <p>1</p>
-            <button className="border border-gray-400 px-2 text-lg rounded-sm">
+            <button className="border px-2 text-lg rounded-sm lg:text-2xl">
+              -
+            </button>
+            <p className="lg:text-2xl">1</p>
+            <button className="border border-gray-400 px-2 text-lg rounded-sm lg:text-2xl">
               +
             </button>
           </div>
         </div>
       </div>
       <hr />
-      <div className="flex items-center gap-10 px-4">
+      <div className="flex items-center gap-10 px-4 md:w-10/12 md:mx-auto md:gap-20 lg:justify-center lg:gap-32">
         <div className="w-2/5">
-          <img src="/assets/gamingheadphone.png" alt="" />
+          <img src="/assets/s22.png" alt="" className="md:h-48" />
         </div>
         <div className="flex flex-col gap-10 py-4">
           <div>
-            <p className="font-bold text-sm">Samsung s22</p>
+            <p className="font-bold text-sm md:text-lg lg:text-2xl">
+              Samsung s22
+            </p>
           </div>
           <div>
-            <p className="font-bold text-orange-500 text-sm">$ 1200</p>
+            <p className="font-bold text-orange-500 text-sm md:text-lg lg:text-2xl">
+              $ 1200
+            </p>
           </div>
         </div>
         <div className="flex flex-col items-center gap-10">
           <div>
-            <button className="text-sm text-red-300">remove</button>
+            <button className="text-sm text-red-300 md:text-lg lg:text-2xl">
+              remove
+            </button>
           </div>
           <div className="flex border justify-between items-center gap-2 p-0 rounded-md ">
-            <button className="border px-2 text-lg rounded-sm">-</button>
-            <p>1</p>
-            <button className="border border-gray-400 px-2 text-lg rounded-sm">
+            <button className="border px-2 text-lg rounded-sm lg:text-2xl">
+              -
+            </button>
+            <p className="lg:text-2xl">1</p>
+            <button className="border border-gray-400 px-2 text-lg rounded-sm lg:text-2xl">
               +
             </button>
           </div>
