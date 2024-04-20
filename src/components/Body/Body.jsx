9b114a29dccd47fd93bfac96f63dd1eb5dc92382
“Peter-Akasi-Mba” <awintechworld@gmail.com>
2024-04-20 09:14:44 +0000

    changed products grid from 1 to 2 cols in mobile view

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -7,7 +7,7 @@ const Body = () => {
     <>
       <div className=" mt-4">
         <hr />
-        <div className="grid md:grid-cols-2 md:gap-4 md:px-4 md:py-5 lg:grid-cols-4 lg:py-10 lg:px-8 ">
+        <div className="grid grid-cols-2 gap-2 p-3 md:grid-cols-2 md:gap-4 md:px-4 md:py-5 lg:grid-cols-4 lg:py-10 lg:px-8 ">
           {products.map((product) => (
             <Product key={product.id} product={product} />
           ))}
--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,29 +1,33 @@
 /* eslint-disable react/prop-types */
 import "./Product.css";
 import { FaHeart } from "react-icons/fa6";
+import { FaCartPlus } from "react-icons/fa";
 
 const Product = ({ product }) => {
   return (
     <>
-      <div className="product relative h-96 mt-4 md:border md:px-2 md:rounded-lg">
+      <div className="product border rounded-md relative h-96  mt-4 md:border md:px-2 md:rounded-lg">
         <div>
-          <img src={product.image} alt={product.name} className="h-72" />
+          <img
+            src={product.image}
+            alt={product.name}
+            className="h-64 object-contain mt-6"
+          />
           <FaHeart className="absolute right-5 top-5 text-red-500 cursor-pointer text-2xl md:text-2xl lg:text-4xl" />
         </div>
-        <div className="flex justify-between items-center px-5">
+        <div className="flex justify-between items-center px-2">
           <div className="flex flex-col gap-2 py-2">
             <p className="font-bold md:text-xl lg:text-2xl">{product.name}</p>
-            <p className="text-orange-600 md:text-lg lg:text-xl">
+            <p className="text-orange-600 font-bold md:text-lg lg:text-xl">
               $ {product.price}
             </p>
           </div>
-          <div>
-            <button className="bg-orange-500 text-white font-bold px-6 py-1 rounded-md border-transparent md:text-lg lg:text-xl">
-              Buy
+          <div className="">
+            <button className="border-2 border-orange-200 px-2 py-2 text-sm md:text-lg rounded-md lg:text-2xl">
+              <FaCartPlus className="text-orange-500 text-xl" />
             </button>
           </div>
         </div>
-        <hr className="w-10/12 mx-auto text-orange-500 md:hidden" />
       </div>
     </>
   );
